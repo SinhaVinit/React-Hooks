4e1e58649503c12429b7eed75a3704e6bb6896cc
SinhaVinit <vinit.8444935532@gmail.com>
2022-02-20 17:25:30 +0530

    Modified App.js and index.js to create auto time updater

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,20 @@
 import React, {useState} from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const now = new Date().toLocaleTimeString();
+  const [time, setTime] = useState(now);
 
-  function increase() {
-    setCount(count+1);
+  function updateTime() {
+    const newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
   }
-  function decrease() {
-    setCount(count-1);
-  }
+  setInterval(updateTime, 1000);
 
+  console.log(time);
   return (
     <div className="container">
-      <h1>{count}</h1>
-      <button onClick={increase}>+</button>
-      <button onClick={decrease}>-</button>
+      <h1>{time}</h1>
+      <button onClick={updateTime}>Get Time</button>
     </div>
   );
 }

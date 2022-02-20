595bd70ad029d2bd8a4796f2d236d43df3eaebc9
SinhaVinit <vinit.8444935532@gmail.com>
2022-02-20 17:02:58 +0530

    Modified App.js to make incresing and decreaseing counter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function increase() {
+    setCount(count+1);
+  }
+  function decrease() {
+    setCount(count-1);
+  }
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{count}</h1>
+      <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
     </div>
   );
 }
